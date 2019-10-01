9f666caadaed7798c83e39e1b19a2d8c48b2639b
Maduoma <aachilefu@yahoo.com>
2019-10-01 05:01:16 +0100

    Python

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
 print('Hello from Python!')
 print()
 name = input('Please what is your name? ')
-print('Your name as provided is: '+name)
+# Capitalizing the first letter in string 
+my_name = name.capitalize()
+print('Your name as provided is: '+my_name)
+print('This is the end of the program. Thanks.')
+
+print('Adding numbers') 
+x = 34 + 4
+print(x)
+print('Dividing by zero') #Us this comment to debug or detect where errors occur
+y = x / 0
+print(y)
+print('This is the end')
