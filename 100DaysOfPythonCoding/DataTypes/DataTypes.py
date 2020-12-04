d428eb88ffb8d126d950a8eff9cad2fd4e811462
maduoma <18477889+maduoma@users.noreply.github.com>
2020-12-04 21:53:54 +0100

    Data Types

--- 100DaysOfPythonCoding/DataTypes/DataTypes.py
+++ 100DaysOfPythonCoding/DataTypes/DataTypes.py
@@ -2,4 +2,16 @@
 # 1. String
 # prints to the console the character at index 4
 print("Hello"[4])
+print("123" + "456")
+
+# 2. Integer
+print(123 + 456)
+print(123_456_789)
+
+# 3. Float
+print(3.12534)
+
+# 4. Boolean
+print(True)
+print(False)
 
