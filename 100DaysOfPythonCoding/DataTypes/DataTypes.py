e9b6d034e7dda6a539dda28041c2235bcf6378e8
maduoma <18477889+maduoma@users.noreply.github.com>
2020-12-04 22:40:59 +0100

    Data Types

--- 100DaysOfPythonCoding/DataTypes/DataTypes.py
+++ 100DaysOfPythonCoding/DataTypes/DataTypes.py
@@ -6,6 +6,7 @@ print("123" + "456")
 
 # 2. Integer
 print(123 + 456)
+# Python ignores the under that serves as commas and prints the number
 print(123_456_789)
 
 # 3. Float
@@ -24,4 +25,10 @@ print(type(num1))
 # Type Conversion or Casting
 print(str(123) + str(456))
 print(float(70))
+print(int(7.2))
+
+# Challenge 1
+two_digit_number = input("Type a two-digit number!\n")
+sum_of_two_digit_num = int(two_digit_number[0]) + int(two_digit_number[1])
+print(sum_of_two_digit_num)
 
