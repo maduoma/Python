8c954067f942de71000da0085b46780740049fc1
maduoma <18477889+maduoma@users.noreply.github.com>
2020-12-05 03:14:29 +0100

    Data Types

---
+++ 100DaysOfPythonCoding/DataTypes/Variables.py
@@ -0,0 +1,7 @@
+#################################################################
+# Challenge 0 : Band Name Generator
+#################################################################
+print("Welcome to Band Name Generator,")
+name = input("What is your name?\n")
+pet = input("What is your pet's name?\n")
+print("Your Band name is " + name + " " + pet)
